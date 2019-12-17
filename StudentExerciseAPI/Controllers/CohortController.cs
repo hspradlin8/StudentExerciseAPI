5da6e13e190e27cdf9c0af2aacf793ed9226d76c
Heidi <hspradlin8@gmail.com>
2019-12-16 22:06:07 -0600

    still working on cohort controller

--- StudentExerciseAPI/Controllers/CohortController.cs
+++ StudentExerciseAPI/Controllers/CohortController.cs
@@ -1,73 +1,115 @@
-﻿//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.Extensions.Configuration;
-//using System.Data;
-//using Microsoft.Data.SqlClient;
-//using StudentExerciseAPI.Models;
-//using Microsoft.AspNetCore.Http;
-//using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using StudentExerciseAPI.Models;
+using Microsoft.AspNetCore.Http;
+using StudentExerciseAPI.Controllers;
 
-//namespace StudentExerciseAPI.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class CohortController : ControllerBase
-//    {
-//        private readonly IConfiguration _config;
+namespace CoffeeShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CohortController : ControllerBase
+    {
+        private readonly IConfiguration _config;
 
-//        public CohortController(IConfiguration config)
-//        {
-//            _config = config;
-//        }
+        public CohortController(IConfiguration config)
+        {
+            _config = config;
+        }
 
-//        public SqlConnection Connection
-//        {
-//            get
-//            {
-//                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
-//            }
-//        }
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
 
-//        [HttpGet]
-//        public async Task<IActionResult> Get()
-//        {
-//            using (SqlConnection conn = Connection)
-//            {
-//                conn.Open();
-//                using (SqlCommand cmd = conn.CreateCommand())
-//                {
-//                    cmd.CommandText = @"SELECT c.Id, c.Name,
-//                                        FROM Cohort c
-//                                        INNER JOIN Cohort c ON c.Id = CohortId";
-//                    SqlDataReader reader = cmd.ExecuteReader();
-//                    List<Cohort> cohorts = new List<Cohort>();
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT c.Id as CohortId, c.Name as CohortName, i.Id as InstructorId, 
+                                        i.FirstName as InstructorFirstName, i.LastName as InstructoLastName, 
+                                        i.Specialty as InstructorSpecialty, i.SlackHandle as InstructorSlack, 
+                                        i.CohortId as InstructorCohortId, s.Id, s.FirstName as StudentFirstName, 
+                                        s.LastName as StudentLastName, s.SlackHandle as StudentSlack, 
+                                        s.CohortId as StudentCohortId
+                                        FROM Cohort c
+                                        LEFT JOIN Student s ON c.Id = s.Id
+                                        LEFT JOIN Instructor i ON c.Id = i.CohortId";
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    List<Cohort> cohorts = new List<Cohort>();
 
-//                    while (reader.Read())
-//                    {
-//                        Cohort cohort = new Cohort
-//                        {
-//                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
-//                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
-//                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
-//                            SlackHandle = reader.GetString(reader.GetOrdinal("SlackHandle")),
-//                            Specialty = reader.GetString(reader.GetOrdinal("Specialty")),
-//                            CohortId = reader.GetInt32(reader.GetOrdinal("CohortId")),
+                    while (reader.Read())
+                    {
+                        int currentCohortID = reader.GetInt32(reader.GetOrdinal("CohortID"));
+                        Cohort newCohort = cohorts.FirstOrDefault(i => i.Id == currentCohortID);
+                        //If there's no cohort, create one and add it to the list.
+                        if (newCohort == null)
 
-//                            Cohort = new CohortController()
-//                            {
+                            newCohort = new Cohort
+                            {
+                                Id = currentCohortID,
+                                Name = reader.GetString(reader.GetOrdinal("CohortName")),
+                                Student = new List<Student>(),
+                                instructors = new List<Instructor>()
 
-//                                Id = reader.GetInt32(reader.GetOrdinal("CoId")),
-//                                Name = reader.GetString(reader.GetOrdinal("Name")),
-//                            }
-//                        };
+                            };
 
-//                        instructors.Add(instructor);
-//                    }
-//                    reader.Close();
+                        cohorts.Add(newCohort);
+                    }
+                    reader.Close();
 
-//                    return Ok(instructors);
-//                }
-//            }
-//        }
-//    }
-//}
+                    return Ok(cohorts);
+                }
+            }
+        }
+
+        //[HttpGet("{id}", Name = "GetEmployee")]
+        //public async Task<IActionResult> Get([FromRoute] int id)
+        //{
+        //    using (SqlConnection conn = Connection)
+        //    {
+        //        conn.Open();
+        //        using (SqlCommand cmd = conn.CreateCommand())
+        //        {
+        //            cmd.CommandText = @"
+        //                SELECT
+        //                    Id, FirstName, LastName, DepartmentId
+        //                FROM Employee
+        //                WHERE Id = @id";
+        //            cmd.Parameters.Add(new SqlParameter("@id", id));
+        //            SqlDataReader reader = cmd.ExecuteReader();
+
+        //            Employee employee = null;
+
+        //            if (reader.Read())
+        //            {
+        //                employee = new Employee
+        //                {
+        //                    Id = reader.GetInt32(reader.GetOrdinal("Id")),
+        //                    FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+        //                    LastName = reader.GetString(reader.GetOrdinal("LastName")),
+        //                    DepartmentId = reader.GetInt32(reader.GetOrdinal("DepartmentId")),
+
+        //                };
+        //            }
+        //            reader.Close();
+
+        //            return Ok(employee);
+        //        }
+        //    }
+        //}
+    }
+}
