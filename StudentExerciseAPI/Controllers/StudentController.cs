d5de9fc098bb00a924c52f6aa29d9f7b9b670857
Heidi <hspradlin8@gmail.com>
2019-12-17 15:24:17 -0600

    part7part1

--- StudentExerciseAPI/Controllers/StudentController.cs
+++ StudentExerciseAPI/Controllers/StudentController.cs
@@ -31,22 +31,51 @@ namespace StudentExerciseAPI.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> Get([FromQuery]int Id, string include)
         {
             using (SqlConnection conn = Connection)
             {
                 conn.Open();
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
-                    cmd.CommandText = @"SELECT s.Id, s.FirstName, s.LastName, s.SlackHandle, s.CohortId, 
-                                        c.Name, c.Id AS CoId
-                                        FROM Student s
-                                        LEFT JOIN Cohort c ON c.Id = CohortId";
-                    SqlDataReader reader = cmd.ExecuteReader();
+                    
                     List<Student> students = new List<Student>();
+                    // if they are looking for exercises
+                    if (include !=null)
+                    
+                    {
+                        cmd.CommandText = @"SELECT s.Id, s.FirstName, s.LastName, s.SlackHandle, s.CohortId, e.Id AS ExerciseId, e.Name AS ExerciseName, e.Language, c.[Name] AS CohortName, c.Id AS CoId
+                                        FROM Student s
+                                        LEFT JOIN Cohort c ON s.CohortId = c.Id
+                                        INNER JOIN  StudentExercise se ON se.StudentId = s.Id
+                                        INNER JOIN Exercise e ON e.Id = se.ExerciseId";
+
+                    }else
+                    {
+                        cmd.CommandText = @"SELECT s.Id, s.FirstName, s.LastName, s.SlackHandle, s.CohortId, c.Id AS CoId, c.Name AS CohortName 
+                                        FROM Student s
+                                        LEFT JOIN  Cohort c ON s.CohortId = c.Id";
+                                       
+                    }
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    //this is where we are storing all of the exercises from the sql statement
+                   
 
                     while (reader.Read())
                     {
+                    List<Exercise> exercises = new List<Exercise>();
+                        if (include != null)
+                        {
+                            Exercise exercise = new Exercise
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Name = reader.GetString(reader.GetOrdinal("ExerciseName")),
+                                Language = reader.GetString(reader.GetOrdinal("Language"))
+
+                            };
+                            exercises.Add(exercise);
+                        }
                         Student student = new Student
                         {
                             Id = reader.GetInt32(reader.GetOrdinal("Id")),
@@ -59,8 +88,11 @@ namespace StudentExerciseAPI.Controllers
                             {
 
                                 Id = reader.GetInt32(reader.GetOrdinal("CoId")),
-                                Name = reader.GetString(reader.GetOrdinal("Name")),
-                            }
+                                Name = reader.GetString(reader.GetOrdinal("CohortName")),
+                            },
+
+                            //this is considered all of the student exercises they are currently working on
+                            Exercise = exercises
                         };
 
                         students.Add(student);
@@ -71,5 +103,6 @@ namespace StudentExerciseAPI.Controllers
                 }
             }
         }
+
     }
 }
